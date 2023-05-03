85147bff715b07b0ec1bb2b48489343b1596d2d8
OlaMag <ale.magdziarek@gmail.com>
2023-05-03 10:01:30 +0200

    handle step forward and backwards

--- src/app.js
+++ src/app.js
@@ -13,10 +13,12 @@ rl.on('line', (line) => {
   let result;
   try {
     const commands = formatStdin(line);
-    result = commands.reduce(
-      (position, command) => (position = move(position, command, size)),
-      start
-    );
+    result = commands.reduce((position, command) => {
+      if (command === 0) {
+        process.exit();
+      }
+      position = move(position, command);
+    }, start);
     rl.on('close', () => {
       console.log(result);
     });
@@ -29,11 +31,67 @@ rl.on('line', (line) => {
 
 function formatStdin(input) {
   try {
-    return input.trim().split(',').map(Number).filter(Boolean);
+    return input
+      .trim()
+      .split(',')
+      .map(Number)
+      .filter((i) => i <= 4 && i >= 0);
   } catch (err) {
     throw `Wrong input: ${err}`;
   }
 }
 
-function move() {}
-module.exports = { move, formatStdin };
+function move(pos, com, size) {
+  if (com === 1) {
+    stepForward(pos);
+  } else if (com === 2) {
+    stepBackwards(pos);
+  } else {
+    rotate(pos, com);
+  }
+}
+
+function stepForward(pos) {
+  let [x, y, z] = pos;
+  switch (z) {
+    case 'N':
+      y--;
+      break;
+    case 'S':
+      y++;
+      break;
+    case 'W':
+      x--;
+      break;
+    case 'E':
+      x++;
+      break;
+    default:
+  }
+  return [x, y, z];
+}
+
+function stepBackwards(pos) {
+  let [x, y, z] = pos;
+  switch (z) {
+    case 'N':
+      y++;
+      break;
+    case 'S':
+      y--;
+      break;
+    case 'W':
+      x++;
+      break;
+    case 'E':
+      x--;
+      break;
+    default:
+  }
+  return [x, y, z];
+}
+function rotate(pos, com) {
+  return;
+}
+
+module.exports = { formatStdin, move, stepForward, stepBackwards, rotate };
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,35 @@
-const { move, formatStdin } = require('../src/app');
+const { formatStdin, stepForward, stepBackwards } = require('../src/app');
 
 describe('stdin is correctly processed ', () => {
   test.each`
-    input             | expected
-    ${'1,2,5,10'}     | ${[1, 2, 5, 10]}
-    ${'1,2,5,10,N,F'} | ${[1, 2, 5, 10]}
+    input                | expected
+    ${'1,2,3,2,4,0'}     | ${[1, 2, 3, 2, 4, 0]}
+    ${'1,2,3,2,4,0,N,F'} | ${[1, 2, 3, 2, 4, 0]}
   `('$input to $expected', ({ input, expected }) => {
     expect(formatStdin(input)).toEqual(expected);
+  });
+});
+
+describe('make a step forward from ', () => {
+  test.each`
+    position       | expected
+    ${[1, 2, 'N']} | ${[1, 1, 'N']}
+    ${[1, 2, 'S']} | ${[1, 3, 'S']}
+    ${[1, 2, 'W']} | ${[0, 2, 'W']}
+    ${[1, 2, 'E']} | ${[2, 2, 'E']}
+  `('$position to $expected', ({ position, expected }) => {
+    expect(stepForward(position)).toEqual(expected);
+  });
+});
+
+describe('make a step backwards from ', () => {
+  test.each`
+    position       | expected
+    ${[1, 2, 'N']} | ${[1, 3, 'N']}
+    ${[1, 2, 'S']} | ${[1, 1, 'S']}
+    ${[1, 2, 'W']} | ${[2, 2, 'W']}
+    ${[1, 2, 'E']} | ${[0, 2, 'E']}
+  `('$position to $expected', ({ position, expected }) => {
+    expect(stepBackwards(position)).toEqual(expected);
   });
 });
